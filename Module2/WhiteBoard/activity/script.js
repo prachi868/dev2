0ee9a51e264deeace1c266280d6181f987b2fbc0
Prachi <jainprachi595@gmail.com>
2022-06-25 22:20:52 +0530

    whiteboard

--- Module2/WhiteBoard/activity/script.js
+++ Module2/WhiteBoard/activity/script.js
@@ -11,3 +11,22 @@ let ctx=canvas.getContext("2d");
 //ctx.fillStyle="yellow";
 //xoffset,yoffset,width,height
 //ctx.fillRect(10,10,150,100);
+let isPenDown=false;
+canvas.addEventListener("mousedown",function(e){
+    isPenDown=true;
+    let x=e.clientX;
+    let y=e.clientY - 100;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+})
+canvas.addEventListener("mousemove",function(e){
+    if(isPenDown){
+        let x=e.clientX;
+        let y=e.clientY - 100;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+})
+canvas.addEventListener("mouseup",function(e){
+    isPenDown=false;
+})
